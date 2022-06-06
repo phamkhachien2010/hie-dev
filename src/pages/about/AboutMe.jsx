9a191744b3473bbb32c03ac69a2748047cabe12d
phamkhachien2010 <phamkhachien17@gmail.com>
2022-06-06 16:18:30 +0700

    i18 about p1

--- src/pages/about/AboutMe.jsx
+++ src/pages/about/AboutMe.jsx
@@ -6,6 +6,7 @@ import { Carousel } from 'antd';
 import { useFormik } from 'formik';
 import { useDispatch } from 'react-redux';
 import { SEND_COMMENT_API } from '../../redux/constant/ConstantSaga';
+import { useTranslation } from 'react-i18next';
 
 const { TabPane } = Tabs;
 
@@ -266,17 +267,18 @@ export default function AboutMe(props) {
     },
   });
 
+  const {t} = useTranslation();
 
   return (
     <div>
       <div style={{ paddingTop: '112px' }} className={`${style.aboutme__content} grid grid-cols-12`}>
         <div className='col-span-8 container py-5 text-center flex flex-col justify-center'>
-          <h1 className='text-4xl '>PHAM KHAC HIEN</h1>
-          <h2 className='text-3xl text-white' >Lập trình viên Full stack</h2>
+          <h1 className='text-4xl '>{t('phamkhachien')}</h1>
+          <h2 className='text-3xl text-white' >{t('fullstackDeveloper')}</h2>
           {/* <p className='text-left text-lg'>I am interested in new technology, always want to change my mindset </p> */}
-          <p className='text-left text-lg'>Mình rất thích tìm hiểu về những công nghệ mới, học thêm những kiến thức về công nghệ, yêu thích sáng tạo, khao khát phát triển bản thân và đặt mục tiêu hoàn thành nhiệm vụ lên hàng đầu.</p>
+          <p className='text-left text-lg'>{t('aboutSlogen')}</p>
           <div className='mt-5'>
-            <NavLink to='/' target='_blank' className='bg-rose-500 border-2 rounded-full px-3 py-2 hover:bg-white hover:text-black hover:no-underline inline-block'>My CV PDF</NavLink>
+            <NavLink to='/' target='_blank' className='bg-rose-500 border-2 rounded-full px-3 py-2 hover:bg-white hover:text-black hover:no-underline inline-block'>{t('mycv')}</NavLink>
           </div>
         </div>
         <div className='col-span-4'>
@@ -287,7 +289,7 @@ export default function AboutMe(props) {
       <section className={style.about__id} id="about">
         <div className={style.about} />
         <div className={style.about__content}>
-          <h2 className='text-center text-3xl font-bold'>About Me</h2>
+          <h2 className='text-center text-3xl font-bold'>{t('about me')}</h2>
           <div className="grid grid-cols-2">
             <div className={`flex justify-center ${style.about_img}`}>
               <img src={require('../../assets/img/avatar2.jpg')} alt="avatar2" />
@@ -295,16 +297,16 @@ export default function AboutMe(props) {
             <div className={`${style.information}`}>
               <ul>
                 <li>
-                  <span className={`${style.infomation__info} ${style.info__text}`}><i className="fa fa-user" /> Name</span> <span className={style.info__text}> Pham Khac Hien</span>
+                  <span className={`${style.infomation__info} ${style.info__text}`}><i className="fa fa-user" /> {t('name')}</span> <span className={style.info__text}>{t('phamkhachien')}</span>
                 </li>
                 <li>
-                  <span className={`${style.infomation__info} ${style.info__text}`}><i className="fa fa-map-marker-alt" /> Adress</span> <span className={style.info__text}> 105 Lang Ha, Dong Da, Ha Noi</span>
+                  <span className={`${style.infomation__info} ${style.info__text}`}><i className="fa fa-map-marker-alt" /> {t('address')}</span> <span className={style.info__text}> {t('addressName')}</span>
                 </li>
                 <li>
                   <span className={`${style.infomation__info} ${style.info__text}`}><i className="fa fa-envelope" /> Email</span> <span className={style.info__text}> phamkhachien17@gmail.com</span>
                 </li>
                 <li>
-                  <span className={`${style.infomation__info} ${style.info__text}`}><i className="fa fa-phone" /> Phone</span> <span className={style.info__text}> 0971948634</span>
+                  <span className={`${style.infomation__info} ${style.info__text}`}><i className="fa fa-phone" /> {t('phone')}</span> <span className={style.info__text}> 0971948634</span>
                 </li>
               </ul>
               <div className="flex">
@@ -321,30 +323,31 @@ export default function AboutMe(props) {
       </section>
 
       <section id="resume" className='py-5' style={{ backgroundImage: 'url(./img/backgroud-resume.jpg)', backgroundAttachment: 'fixed', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
-        <h2 className='text-center text-3xl font-bold'>My Resume</h2>
+        <h2 className='text-center text-3xl font-bold'>{t('resume')}</h2>
         <div className={style.resume__content}>
           <div className="grid grid-cols-2 lg:grid-cols-1 text-base">
             <div>
               <div className={style.experience}>
-                <h5 className="font-bold">EXPERIENCE</h5>
+                <h5 className="font-bold mb-0">{t('experience')}</h5>
                 <hr />
-                <p>- Learning and practicing with many Front-end projects</p>
-                <p>- Working on my own projects with learned Front-end skills</p>
-                <p>- Proficient in use Redux and some libraries relate to ReactJs</p>
-                <p>- Actually I don't have much experience at front-end developer. But I have passion for technology, and wish for change my skills. And I hope I can do something with my ability to your company's development</p>
+                <p>- {t('exper1')}</p>
+                <p>- {t('exper2')}</p>
+                <p>- {t('exper3')}</p>
+                <p>- {t('exper4')}</p>
+                <p>- {t('exper-end')}</p>
               </div>
             </div>
             <div>
               <div className={style.education}>
-                <h5 className="font-bold">EDUCATION</h5>
+                <h5 className="font-bold mt-3 mb-0">{t('education')}</h5>
                 <hr />
-                <p className="font-bold">Cybersoft - Cyberlearn Programing Education Center</p>
-                <p>- Student of Cybersoft (8/2021 - 4/2022)</p>
-                <p>- Programming course: thinking in programming, problem-solving, Java language, OOP</p>
-                <p>- Front-end Foundation course: basic knowledge of HTML5, CSS3, Bootstrap 4, SASS/SCSS, Javascript (ES5/ES6), Jquery, AJAX, Git, ...</p>
-                <p>- Front End Master ReactJS course: base ReactJs, React class component, React functional component, life-circle, React function hooks, redux, AJAX and some libraries</p>
-                <p className="font-bold">Military Academy of Logistics (2015-2019)</p>
-                <p>- Studied and trained as a soilder of the VietNam’s Army</p>
+                <p className="font-bold">{t('cybersoftLearning')}</p>
+                <p>- {t('timeLearnOnCyber')}</p>
+                <p>- {t('programCourse')}</p>
+                <p>- {t('frontendcourse')}</p>
+                <p>- {t('frontendReact')}</p>
+                <p className="font-bold">{t('MALStudy')}</p>
+                <p>- {t('learnOnMAL')}</p>
               </div>
             </div>
           </div>
@@ -353,14 +356,14 @@ export default function AboutMe(props) {
 
       <section className={`${style.skills__id} py-5`} id="skills" style={{ backgroundImage: 'url(./img/background-skill.jpg)', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
         <div className="container text-base">
-          <h2 className='text-center text-3xl font-bold'>My Skills</h2>
+          <h2 className='text-center text-3xl font-bold'>{t('skill')}</h2>
           {renderMySkill()}
         </div>
       </section>
 
       <section className='container py-5'>
-        <h2 className='text-center text-3xl font-bold'>My projects</h2>
-        <h6 className='text-center text-lg'>There are my projects, that i did when i learn at <a href="https://cyberlearn.vn/" target='_blank' className='text-blue-500'>Cyberlearn</a></h6>
+        <h2 className='text-center text-3xl font-bold'>{t('project')}</h2>
+        <h6 className='text-center text-lg'>{t('projectSlog')} <a href="https://cyberlearn.vn/" target='_blank' className='text-blue-500'>Cyberlearn</a></h6>
         <Tabs defaultActiveKey="1">
           {renderTabProject()}
         </Tabs>
@@ -368,11 +371,11 @@ export default function AboutMe(props) {
 
       <section className='contact__form py-5' style={{ backgroundImage: 'url(https://img.freepik.com/free-photo/old-black-background-grunge-texture-dark-wallpaper-blackboard-chalkboard-room-wall_1258-28312.jpg?size=626&ext=jpg&uid=R71559152&ga=GA1.2.232571487.1602147622)', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
         <div className='container'>
-          <h2 className='text-center text-3xl font-bold text-white'>Contact with me</h2>
+          <h2 className='text-center text-3xl font-bold text-white'>{t('contact')}</h2>
           <Form
             {...formItemLayout}
-            scrollToFirstError
             onSubmitCapture={formik.handleSubmit}
+            scrollToFirstError
           >
             <Form.Item
               name="email"
@@ -380,11 +383,11 @@ export default function AboutMe(props) {
               rules={[
                 {
                   type: 'email',
-                  message: 'email không hợp lệ!',
+                  message: `${t('emailValid')}`,
                 },
                 {
                   required: true,
-                  message: 'Email không nên bỏ trống!',
+                  message: `${t('emailRequire')}`,
                 },
               ]}
             >
@@ -393,11 +396,11 @@ export default function AboutMe(props) {
 
             <Form.Item
               name="name"
-              label="Tên của bạn"
+              label={t('name')}
               rules={[
                 {
                   required: true,
-                  message: 'Mình muốn biết tên của bạn!',
+                  message: `${t('nameValid')}`,
                   whitespace: true,
                 },
               ]}
@@ -411,7 +414,7 @@ export default function AboutMe(props) {
               rules={[
                 {
                   required: true,
-                  message: 'Comment ở đây nhé!',
+                  message: `${t('commentValid')}`,
                 },
               ]}
             >
@@ -419,7 +422,7 @@ export default function AboutMe(props) {
             </Form.Item>
             <Form.Item {...tailFormItemLayout}>
               <Button type="primary" htmlType="submit">
-                Gửi
+                {t('send')}
               </Button>
             </Form.Item>
           </Form>
